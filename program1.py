d0c1f1681343585ed37096250ed697393a7a39cb
Hemanth4478 <123075496+Hemanth4478@users.noreply.github.com>
2024-11-11 14:13:52 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,27 @@
-class Solution:
-   
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+from collections import deque
+def numIsIands(grid):
+   if not grid:
+      return 0
+
+   def bfs(grid,i,j):
+      queue=deque([(i, j)])
+      grid[i][j]='W'
+
+      directions=[(0,1),(0,-1),(1,0),(-1,0)]
+      while queue:
+         x,y =queue.popleft()
+         for dx, dy in directions:
+            nx,ny=x+dx, y+dy
+
+         if 0<=nx <len(grid) and 0 <=ny <len(grid[0]) and grid[nx][ny]=='L':
+            grid[nx][ny]='W'
+            queue.append((nx,ny))
+
+num_islands=0
+for i in range(len(grid)):
+   for j in range(len(grid[0])):
+      if grid[i][j]=='L':
+         num_islands +=1
+         bfs(grid,i,j)
+
+return num_islands
